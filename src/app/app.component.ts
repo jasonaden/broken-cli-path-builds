270343cedb537830eac40c192a895e9dbe51feeb
Jason Aden <jasonaden@google.com>
2018-01-23 13:20:42 -0800

    add build repro steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HighlightDirective} from 'ng-highlight';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material';
+
+import {HighlightModule} from 'ng-highlight';
 
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTabsModule,
+    NoopAnimationsModule,
+    HighlightModule
   ],
   providers: [],
   bootstrap: [AppComponent]
